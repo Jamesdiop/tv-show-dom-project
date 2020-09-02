658bfcba50c6dd89303c6d816a7c396938fbdde2
Jacques Vertommen <jacquesvertommen@gmail.com>
2020-09-02 23:42:14 +0100

    first commit

--- script.js
+++ script.js
@@ -6,7 +6,26 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // create content for each movie card
+  episodeList.forEach((element) => {
+    const cardMovie = document.createElement("div");
+    const moviePoster = document.createElement("img");
+    const summary = document.createElement("p");
+
+    moviePoster.setAttribute("src", `${element.image.medium}`);
+
+    rootElem.appendChild(cardMovie);
+    cardMovie.appendChild(moviePoster);
+    rootElem.appendChild(summary);
+
+    cardMovie.classList.add("card");
+    cardMovie.innerHTML += element.name;
+    cardMovie.innerHTML += element.summary;
+  });
 }
 
 window.onload = setup;
+
+// const episodes = getAllEpisodes();
+// episodes.forEach(element => console.log(element.name, element.season));
