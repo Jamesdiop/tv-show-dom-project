90fccbd7bedb880c1ecbac62a905f8499467dcb2
Jacques Vertommen <jacquesvertommen@gmail.com>
2020-09-17 00:34:22 +0100

    Level 200 Done

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+const rootElem = document.querySelector('#root');
 
-// search bar and label
+// search bar 
 const body = document.querySelector('body');
 const searchBar = document.createElement('input');
 searchBar.setAttribute('type', 'text');
@@ -13,13 +10,21 @@ searchBar.setAttribute('size', '50');
 searchBar.setAttribute('style', 'height:2rem, font-size:1.4rem');
 body.prepend(searchBar);
 
+  // Count episodes and display them
+  const countEpisode = document.createElement("p");
+  body.prepend(countEpisode);
+ 
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  displayAllEpisodes(episodeList);
+  searchEpisodes();
+}
 
+function displayAllEpisodes(episodeList) {
   // create content for each movie card
-  episodeList.forEach((element) => {
+  return episodeList.forEach((element) => {
     //create different tags
     const linkToMovie = document.createElement('a');
     const cardMovie = document.createElement("div");
@@ -36,9 +41,9 @@ function makePageForEpisodes(episodeList) {
     cardMovie.classList.add("card");
     heading.classList.add('heading');
     moviePoster.classList.add('moviePoster');
-    // add href attribute to the link
+    // add link to the tv maze.com
     linkToMovie.href = element.url;
-    //url
+    
     moviePoster.setAttribute("src", `${element.image.medium}`);    
     // display text for heading, summary, episode code
       const episodeCode = `S0${element.season}E0${element.number}`;
@@ -47,19 +52,50 @@ function makePageForEpisodes(episodeList) {
   });
 }
 
+// Search episodes
+function searchEpisodes() {
+  const allEpisodes = getAllEpisodes(); 
+  let input = searchBar.value.toLowerCase();
+  if(input.length === 0) {
+    return allEpisodes;
+  }
+  const newEpisodesList = [];
+  for(let i = 0; i < allEpisodes.length; i++) {
+    let episodesText = allEpisodes[i].name.toLowerCase() + allEpisodes[i].summary.toLowerCase();
+    if (episodesText.includes(input)) {
+      newEpisodesList.push(allEpisodes[i]);
+    }
+  }
+  console.log(newEpisodesList);
+  
+  return newEpisodesList;
+}
 
-searchBar.addEventListener("keyup", function(e){
-      //access to the input value
-      const textEnter =  e.target.value.toLowerCase();
-      const episodeText;
-      for (let i=0; i<getAllEpisodes().length; i++) {
-        episodeTxt = getAllEpisodes()[i].name + getAllEpisodes()[i].summary;
-        if(textEnter.indexOf(episodeList) !== -1) {
-          
-        }
-      }
-      
-});
+function displayEpisodesFound() {
+  rootElem.innerHTML = "";
+  const episodesFound = searchEpisodes();
+  console.log(displayAllEpisodes(episodesFound));
 
+
+  // countEpisode.textContent = `Got ${episodesFound.length} / ${episodeList.length} episode(s)`;
+  countEpisode.textContent = `Got ${episodesFound.length} / ${getAllEpisodes().length} episode(s)`;
+
+  return displayAllEpisodes(episodesFound);
+}
+
+// function displayNumOfEpisodes(episodes) {
+//   // Count episodes and display them
+  
+//   return countEpisode.textContent = `Got ${episodesFound.length} / ${episodeList.length} episode(s)`;
+// }
+
+
+function setup() {
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
+  searchBar.addEventListener("keyup", displayEpisodesFound);
+
+  countEpisode.textContent = `Got ${allEpisodes.length} / ${allEpisodes.length} episode(s)`;
+}
 
 window.onload = setup;
